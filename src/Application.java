f7914a3d7b4be5bfe523951d7cc53260d989f344
Ronaldo-Junior1 <ronaldosjr2005@gmail.com>
2023-08-19 18:04:41 -0300

    T001: Implementação dos metodos: 'addTask' e 'saveTasksToFile'

--- src/Application.java
+++ src/Application.java
@@ -1,5 +1,52 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Task> tasksList = new ArrayList<>(); // Crie uma lista vazia de tarefas
+        TaskManager taskManager = new TaskManager(tasksList);
 
+
+
+        int escolha;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Adicionar Tarefa");
+            System.out.println("2. Listar Tarefas por Categoria");
+            System.out.println("3. Listar Tarefas por Prioridade");
+            System.out.println("4. Listar Tarefas por Status");
+            System.out.println("5. Remover Tarefa");
+            System.out.println("6. Sair");
+            System.out.print("Escolha uma opção: ");
+            escolha = scanner.nextInt();
+
+            switch (escolha) {
+                case 1:
+                    System.out.print("Nome da tarefa: ");
+                    String nome = scanner.next();
+                    System.out.print("Descrição: ");
+                    String descricao = scanner.next();
+                    System.out.print("Data de término (dd/mm/aaaa): ");
+                    String dueDate = scanner.next();
+                    LocalDate dataEntrega = LocalDate.parse(dueDate, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+                    System.out.print("Prioridade (1 a 5): ");
+                    int prioridade = scanner.nextInt();
+                    System.out.print("Categoria: ");
+                    String categoria = scanner.next();
+                    System.out.print("Status (todo, doing, done): ");
+                    String status = scanner.next();
+
+                    Task newTask = new Task(nome, descricao, dataEntrega, prioridade, categoria, status);
+                    taskManager.addTask(newTask);
+                    taskManager.saveTasksToFile("src/Task.txt");
+
+                    System.out.println("Tarefa adicionada com sucesso!");
+                    break;
+
+            }
+        }while (escolha != 6);
+        }
     }
-}
--- src/TaskManager.java
+++ src/TaskManager.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 public class TaskManager {
@@ -7,5 +9,37 @@ public class TaskManager {
         this.tasks = tasks;
     }
 
-
+    public void addTask(Task task) {
+        if (tasks.isEmpty()) {
+            tasks.add(task);
+        } else {
+            int insertIndex = 0;
+            for (int i = 0; i < tasks.size(); i++) {
+                if (task.getPrioridade() > tasks.get(i).getPrioridade()) {
+                    insertIndex = i + 1;
+                } else {
+                    break;
+                }
+            }
+            tasks.add(insertIndex, task);
+        }
     }
+
+    public void saveTasksToFile(String filename) {
+        try (FileWriter writer = new FileWriter(filename)) {
+            for (Task task : tasks) {
+                writer.write(task.getNome() + ", " +
+                        task.getDescricao() + ", " +
+                        task.getDataEntrega() + ", " +
+                        task.getPrioridade() + ", " +
+                        task.getCategoria() + "," +
+                        task.getStatus() + "\n");
+            }
+            System.out.println("Tarefas salvas no arquivo " + filename);
+        } catch (IOException e) {
+            System.err.println("Erro ao salvar as tarefas no arquivo: " + e.getMessage());
+        }
+    }
+
+
+}
